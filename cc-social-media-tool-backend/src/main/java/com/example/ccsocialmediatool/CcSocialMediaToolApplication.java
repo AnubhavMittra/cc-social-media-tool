3b130429f13939d0e57ab1bbd46cadb50b27e897
AnubhavMittra <i.am.anubhav.mittra@gmail.com>
2023-12-14 17:31:46 +0530

    Created smt backend application with basic configuration

---
+++ cc-social-media-tool-backend/src/main/java/com/example/ccsocialmediatool/CcSocialMediaToolApplication.java
@@ -0,0 +1,13 @@
+package com.example.ccsocialmediatool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CcSocialMediaToolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CcSocialMediaToolApplication.class, args);
+	}
+
+}
---
+++ cc-social-media-tool-backend/src/test/java/com/example/ccsocialmediatool/CcSocialMediaToolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ccsocialmediatool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CcSocialMediaToolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
